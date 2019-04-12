6c3c68d8516f8f017f89c1a784789fadeabd58e4
Aruna Benjamin <arunabenji29@gmail.com>
2019-04-12 11:57:40 -0600

    incomplete stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,52 @@
 class Carousel {
+    constructor(carouselElement){
+        this.carouselElement = carouselElement;
+        // console.log(this.carouselElement);
+        this.right = this.carouselElement.querySelector('.right-button');
+        // console.log('right button' ,this.right)
+        this.left = this.carouselElement.querySelector('.left-button');
+        // console.log('left button' ,this.left)
+        this.images = this.carouselElement.querySelectorAll('img')
+        console.log('Images length',this.images.length)
+        this.images.forEach(eachImage => console.log(eachImage))
+        // console.log('all images',this.images)
+        this.currentIndex = 0;
+
+        this.right.addEventListener('click', () => this.increment())
+        this.left.addEventListener('click',() => this.decrement())
+    }
+
+    
+
+    increment(){
+        console.log('this works')
+        this.currentIndex +=1;
+        
+        if (this.currentIndex >= this.images.length){
+            this.currentIndex = 0;
+        }
+        console.log('currentindex' ,this.currentIndex)
+        console.log('current image',this.images[this.currentIndex])
+        
+    }
+
+    decrement(){
+        console.log('this works')
+        this.currentIndex -=1;
+        
+        if (this.currentIndex < 0){
+            this.currentIndex = this.images.length-1;
+        }
+
+        console.log('currentindex' ,this.currentIndex)
+        console.log('current image',this.images[this.currentIndex])
+    }
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+
+new Carousel(carousel)
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
